f7d8180ab71123e553f50d157db892b513b34362
成 <137651076@qq.com>
2020-11-02 21:18:59 +0800

    test

--- KdyWeb.NetCore/KdyWeb.Test/TestStartup.cs
+++ KdyWeb.NetCore/KdyWeb.Test/TestStartup.cs
@@ -81,7 +81,6 @@ namespace KdyWeb.Test
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
           
