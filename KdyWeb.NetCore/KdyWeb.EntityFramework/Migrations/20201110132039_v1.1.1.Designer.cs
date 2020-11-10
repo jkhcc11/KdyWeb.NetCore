494d2e9011258de1eed1f22fee7d1aedc1ec2aa9
成 <137651076@qq.com>
2020-11-10 21:24:20 +0800

    数据迁移

--- KdyWeb.NetCore/KdyWeb.EntityFramework/Migrations/20201110132039_v1.1.1.Designer.cs
+++ KdyWeb.NetCore/KdyWeb.EntityFramework/Migrations/20201110132039_v1.1.1.Designer.cs
@@ -4,14 +4,16 @@ using KdyWeb.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KdyWeb.EntityFramework.Migrations
 {
     [DbContext(typeof(ReadWriteContext))]
-    partial class ReadWriteContextModelSnapshot : ModelSnapshot
+    [Migration("20201110132039_v1.1.1")]
+    partial class v111
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
