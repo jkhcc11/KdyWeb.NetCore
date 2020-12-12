3c8ef4af61e7646a51a926539638fdab6bb0a8f4
成 <137651076@qq.com>
2020-12-12 19:34:03 +0800

    影片系列调整

--- KdyWeb.NetCore/KdyWeb.EntityFramework/Migrations/20201212090014_VideoSeriesList.Designer.cs
+++ KdyWeb.NetCore/KdyWeb.EntityFramework/Migrations/20201212090014_VideoSeriesList.Designer.cs
@@ -4,14 +4,16 @@ using KdyWeb.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KdyWeb.EntityFramework.Migrations
 {
     [DbContext(typeof(ReadWriteContext))]
-    partial class ReadWriteContextModelSnapshot : ModelSnapshot
+    [Migration("20201212090014_VideoSeriesList")]
+    partial class VideoSeriesList
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
