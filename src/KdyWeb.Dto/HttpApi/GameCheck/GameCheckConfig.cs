7d4add1e76930dec388ff79e977ae05abc469701
成 <137651076@qq.com>
2022-08-28 00:22:50 +0800

    bbs 自定义ua

--- src/KdyWeb.Dto/HttpApi/GameCheck/GameCheckConfig.cs
+++ src/KdyWeb.Dto/HttpApi/GameCheck/GameCheckConfig.cs
@@ -26,5 +26,11 @@ namespace KdyWeb.Dto.HttpApi.GameCheck
         /// BBS salt
         /// </summary>
         public string BbsSalt { get; set; }
+
+        /// <summary>
+        /// Bbs ua
+        /// </summary>
+        public string BbsUserAgent { get; set; } =
+            "Mozilla/5.0 (Linux; Android 12; RMX3350 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36";
     }
 }
