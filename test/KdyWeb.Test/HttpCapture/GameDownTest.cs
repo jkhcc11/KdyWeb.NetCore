72591d931eba677f2d57655936238707f6e6ecc3
成 <137651076@qq.com>
2022-12-07 23:37:40 +0800

    byrut bug fix

--- test/KdyWeb.Test/HttpCapture/GameDownTest.cs
+++ test/KdyWeb.Test/HttpCapture/GameDownTest.cs
@@ -8,12 +8,12 @@ namespace KdyWeb.Test.HttpCapture
     [TestClass]
     public class GameDownTest : BaseTest<IGameDownWithByrutService>
     {
-        private const string Cookie = "cf_clearance=aaysrsEU9t6MzLjIMUTww5A6zv5eOFLRngAEE4jB.6g-1670325979-0-160;__cf_bm=8Z4NDEuYGH0UBhvvrd9b3YJRfKeSJKTuhn4Wj03imNs-1670325985-0-Ado1D6IfX3RnINqXmwGUGOxsRn1jyVNl63kalkyGVm/ezloVYLMMtEs1bCMUIlHDxYmWDKIu2t8ohcLWHvKfnHv/V4nL1/vkW4+Jb7tPO/wfPcwDKIVY/EWWBBh3QRN6g+FI2Jpt/qpm5OBfWsC+5Sg=";
+        private const string Cookie = "__cf_bm=qWBgGHK8yOncTgv5HjtFiCW20NpwvbVH1ZK3S89YIHE-1670424388-0-AbjXCvM9a4AaMQK8EQ2KCwv3gj7f93nuCpNYubRvYBGulcnKMECNynytNACmf/c2JiXwZPUqZUHVOpHnxvqDm1WYXhqrNN7qXHJgckOubPqr/s2HV6oA+Cf5RuBExj0Al6HlBzCTbCViWbD+5oTFP80=;cf_clearance=C6CnYAcXYXn6vUxbc8S8RMpbBozV.LaLvQke7EOi69k-1670424398-0-160";
 
         [TestMethod]
         public async Task DetailUrl()
         {
-            await _service.CreateDownInfoByDetailUrlAsync("https://byrut.org/14330-evil-west.html"
+            await _service.CreateDownInfoByDetailUrlAsync("https://byrut.org/29309-mini-battle-ground.html"
             , "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
             , Cookie);
             Assert.IsTrue(true);
