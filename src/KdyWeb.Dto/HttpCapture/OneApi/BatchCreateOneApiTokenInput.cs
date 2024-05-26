2e2c6fc5eaf67946a0ca612d89d0610bd848c464
成 <137651076@qq.com>
2024-05-26 17:16:24 +0800

    新增one-api令牌管理

---
+++ src/KdyWeb.Dto/HttpCapture/OneApi/BatchCreateOneApiTokenInput.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace KdyWeb.Dto.HttpCapture
+{
+    /// <summary>
+    /// 批量创建Token
+    /// </summary>
+    public class BatchCreateOneApiTokenInput
+    {
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int TokenCount { get; set; }
+
+        /// <summary>
+        /// Api密钥
+        /// </summary>
+        [Required(ErrorMessage = "ApiToken必填")]
+        public string ApiToken { get; set; }
+
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        [DataType(DataType.Date, ErrorMessage = "日期格式错误")]
+        public DateTime ExpiredTime { get; set; }
+
+        /// <summary>
+        /// 过期时间input
+        /// </summary>
+        [JsonProperty("expired_time")]
+        public long ExpiredTimeExt { get; set; }
+
+        [Required(ErrorMessage = "名称前缀必填")]
+        public string TokenNamePrefix { get; set; }
+
+        /// <summary>
+        /// Token名
+        /// </summary>
+        [JsonProperty("name")]
+        public string TokenName { get; set; }
+
+        /// <summary>
+        /// 可用额度
+        /// </summary>
+        /// <remarks>
+        ///  1刀 50W
+        /// </remarks>
+        [JsonProperty("remain_quota")]
+        [Range(1, 50)]
+        public int RemainQuota { get; set; }
+
+        /// <summary>
+        /// 是否无限制额度
+        /// </summary>
+        [JsonProperty("unlimited_quota")]
+        public bool UnlimitedQuota { get; set; }
+    }
+}
---
+++ src/KdyWeb.Dto/HttpCapture/OneApi/BatchCreateOneApiTokenOut.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace KdyWeb.Dto.HttpCapture
+{
+    /// <summary>
+    /// 批量创建Token
+    /// </summary>
+    public class BatchCreateOneApiTokenOut
+    {
+        /// <summary>
+        /// Token
+        /// </summary>
+        public string Key { get; set; }
+
+        public string Token => "sk-" + Key;
+    }
+}
