02c3a058666236cb7df2e9a9405ba348919e3344
FishPC <137651076@qq.com>
2025-08-20 02:17:10 +0800

    新增接龙奖励map

---
+++ src/KdyWeb.EntityFramework/Mapping/SequenceRecord/GiftUserConfigMap.cs
@@ -0,0 +1,15 @@
+﻿using KdyWeb.Entity.SequenceRecord;
+
+namespace KdyWeb.EntityFramework.Mapping.SequenceRecord
+{
+    /// <summary>
+    /// 奖励用户配置 Map
+    /// </summary>
+    public class GiftUserConfigMap : KdyBaseMap<GiftUserConfig, long>
+    {
+        public GiftUserConfigMap() : base("SequenceRecord_GiftUserConfig")
+        {
+
+        }
+    }
+}
---
+++ src/KdyWeb.EntityFramework/Mapping/SequenceRecord/VenuesConfigMap.cs
@@ -0,0 +1,15 @@
+﻿using KdyWeb.Entity.SequenceRecord;
+
+namespace KdyWeb.EntityFramework.Mapping.SequenceRecord
+{
+    /// <summary>
+    /// 场馆配置 Map
+    /// </summary>
+    public class VenuesConfigMap : KdyBaseMap<VenuesConfig, long>
+    {
+        public VenuesConfigMap() : base("SequenceRecord_VenuesConfig")
+        {
+
+        }
+    }
+}
